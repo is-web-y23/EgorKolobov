1593142b03b789884429ecd8abc74f8147187cdc
Egor <egorius@MacBook-Air-Egor.local>
2022-04-12 23:34:53 +0300

    add lab2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,37 +1,36 @@
 import { Get, Controller, Render, UseInterceptors } from "@nestjs/common";
-// import { APP_INTERCEPTOR } from "@nestjs/core";
-// import { AppInterceptor } from './app.interceptor';
+import { AppInterceptor } from "./app.interceptor";
 
-@Controller()
-// @UseInterceptors(new AppInterceptor())
+@Controller() // - чтобы логин работал
+// @UseInterceptors(AppInterceptor) // - чтобы работало время сервера
 export class AppController {
   @Get()
-  @Render('index')
+  @Render("index")
   root() {
-    return;
+    return { username: "Egor" };
   }
 
-  @Get('chat')
-  @Render('chat')
+  @Get("chat")
+  @Render("chat")
   root2() {
-    return;
+    return { username: "Egor" };
   }
 
-  @Get('table')
-  @Render('table')
+  @Get("table")
+  @Render("table")
   root3() {
-    return;
+    return { username: "Egor" };
   }
 
-  @Get('table_form')
-  @Render('table_form')
+  @Get("table_form")
+  @Render("table_form")
   root4() {
-    return;
+    return { username: "Egor" };
   }
 
-  @Get('register')
-  @Render('register')
-  root5() {
-    return;
+  @Get("register")
+  @Render("register.hbs")
+  register() {
+    return { username: 'Egor' };
   }
 }
